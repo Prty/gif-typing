635afd7663c1c94d215ae9331908fb39faf96bae
tomgalle <admin@TOMGALLECOM.local>
2014-07-22 17:03:24 -0400

    added 'bad' characters

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,5 @@
 (function () {
 
-	// $('textarea').on("click", function(){
-	// 	setInterval ('cursorAnimation()', 1200);
-	// });
-
-
-	// function cursorAnimation() {
- //    $('.cursor').animate({
-	//         opacity: 0
-	//     }, 'fast', 'swing').animate({
-	//         opacity: 1
-	//     }, 'fast', 'swing');
-	// };
 
 	var textArea = [],
 		textAreaLength = 0,
@@ -21,7 +9,7 @@
 		var currentString = $(this).val();
 		var currentStringArray = $(this).val().split('');
 		var lastChar = currentStringArray[currentStringArray.length - 1];
-		var badChar = ['[' ];
+		var badChar = ['`','1','2','3','4','5','6','7','8','9','0','-','=','[',']',';','.','/','!','@','#','$','%','^','&','*','(',')','_','+','{','}',':','"','<','>','?',"'"];
 		var inArray = isInArray(lastChar, badChar);
 
 		
@@ -50,70 +38,5 @@
 
 
 })();
-
-
-
-
-
-// 	var captionLength = 0;
-// 	var caption = '';
-
-
-// $(document).ready(function() {
-//     setInterval ('cursorAnimation()', 600);
-//     captionEl = $('#caption');
-    
-//     $('#test-typing').click(function(){
-//         testTypingEffect();
-//     });
-
-//     $('#test-erasing').click(function(){
-//         testErasingEffect();
-//     });
-// });
-
-// function testTypingEffect() {
-//     caption = $('input#user-caption').val();
-//     type();
-// }
-
-// function type() {
-//     captionEl.html(caption.substr(0, captionLength++));
-//     if(captionLength < caption.length+1) {
-//         setTimeout('type()', 50);
-//     } else {
-//         captionLength = 0;
-//         caption = '';
-//     }
-// }
-
-// function testErasingEffect() {
-//     caption = captionEl.html();
-//     captionLength = caption.length;
-//     if (captionLength>0) {
-//         erase();
-//     } else {
-//         $('#caption').html("You didn't write anything to erase, but that's ok!");
-//         setTimeout('testErasingEffect()', 1000);
-//     }
-// }
-
-// function erase() {
-//     captionEl.html(caption.substr(0, captionLength--));
-//     if(captionLength >= 0) {
-//         setTimeout('erase()', 50);
-//     } else {
-//         captionLength = 0;
-//         caption = '';
-//     }	
-// }
-
-// function cursorAnimation() {
-//     $('#cursor').animate({
-//         opacity: 0
-//     }, 'fast', 'swing').animate({
-//         opacity: 1
-//     }, 'fast', 'swing');
-// }
 
 
