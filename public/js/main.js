91a2c322ed91aa5aa93d994ff1b14cdf7aa3a271
Moises Sanabria <sesmoi.data@gmail.com>
2014-07-22 11:36:05 -0400

    adding new changes for gif type

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,27 @@
 (function () {
-	$(document).ready(function() {
-		$('textarea.email').tokenField();
+
+	var textArea = [],
+		textAreaLength = 0,
+		pictypeArea = $('.pictype-area');
+
+	$('.textarea').on('input',function(e) {
+		var currentString = $(this).val();
+		var currentStringArray = $(this).val().split('');
+		var lastChar = currentStringArray[currentStringArray.length - 1];
+
+		if (lastChar === ' ') {
+			var elementToAppend = '<div class="pictype-char ' + lastChar + '"></div>';
+		} else {
+			var elementToAppend = '<div class="pictype-char ' + lastChar + '"><img src="assets/gif-font/eiji/' + lastChar + '.gif?id=' + Math.random() + '"/></div>';
+		}
+		
+		if (currentStringArray.length > textAreaLength ) {
+			pictypeArea.append(elementToAppend);
+			textAreaLength = currentStringArray.length;
+		} else {
+			console.log($('.pictype-char').last());
+			$('.pictype-char').last().remove();
+			textAreaLength = currentStringArray.length;
+		}
 	});
 })();
